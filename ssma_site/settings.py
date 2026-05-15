a271be7e5cc1de8a738e783117dc02938b1346d7
Samuel Yudi <toyotaa038@gmail.com>
2026-05-14 23:26:55 -0400

    Remove PostgreSQL database dependency

--- ssma_site/settings.py
+++ ssma_site/settings.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 
-import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,22 +71,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "ssma_site.wsgi.application"
 
-database_url = os.environ.get("DATABASE_URL")
-
-if database_url:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            database_url,
-            conn_max_age=600,
-        )
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 AUTH_PASSWORD_VALIDATORS = []
 
